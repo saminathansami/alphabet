8a5c7ef1944ca269e123ceaf69a507919d19c202
saminathansami <42169813+saminathansami@users.noreply.github.com>
2019-07-02 16:27:37 +0530

    Create longestcommonprefix.py

---
+++ longestcommonprefix.py
@@ -0,0 +1,12 @@
+sam=int(input())
+a=[]
+for i in range(0,sam):
+ ram=input()
+ b.append(ram)
+li=[]
+for i in zip(*a):
+ if(i.count(i[0])==len(i)):
+  zi.append(i[0])
+ else:
+  break
+print("  ".join(zi))
