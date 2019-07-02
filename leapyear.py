62e556ea8ea96d97ced3deb1bf561f04722ff01f
saminathansami <42169813+saminathansami@users.noreply.github.com>
2019-07-02 16:06:48 +0530

    Create leapyear.py

---
+++ leapyear.py
@@ -0,0 +1,10 @@
+ly=int(input("  "))
+if ly%100==0:
+       if ly%400==0:
+           print ('yes')
+       else:
+          print ('no')
+elif (ly%4==0):
+          print ('yes')
+else:
+         print ('no')
